79d54ef958f431c62b90d948c32a99bf585c7d2f
Ben <manglebot@users.noreply.github.com>
2023-10-20 12:37:27 +0100

    fix:imperative_vs_declarative refactor to use forEach loop

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -3,11 +3,11 @@
 export function longPasswords(passwords) {
   let longPasswords = [];
 
-  for (let i = 0; i < passwords.length; i++) {
-    const password = passwords[i];
+  passwords.forEach(password => {
     if (password.length >= 9) {
       longPasswords.push(password);
     }
-  }
+  });
+
   return longPasswords;
 }
